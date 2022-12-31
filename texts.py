34a6678dcb5e5943cf2bce1eae749454a7e455ab
Vijay14958 <116162193+Vijay14958@users.noreply.github.com>
2022-12-31 10:30:21 +0530

    Update texts.py

--- texts.py
+++ texts.py
@@ -1,7 +1,7 @@
-HELP_TEXT = """👊Cᴏɴᴛᴀᴄᴛ ʀᴇᴘᴏʀᴛ ʙᴜɢ :-@BotCreator99"""
+HELP_TEXT = """👊Cᴏɴᴛᴀᴄᴛ ʀᴇᴘᴏʀᴛ ʙᴜɢ :-@Anjel_Neha"""
 
 ABOUT_TEXT = """
 <b>⚜ Mʏ ɴᴀᴍᴇ :- ShareusBypasserBot</b>\n
-<b>🔸Vᴇʀꜱɪᴏɴ :- <a href='https://telegram.me/BotMinister'>1.0</a></b>\n
-<b>🔹Dᴇᴠᴇʟᴏᴘᴇʀ :- <a href='https://t.me/BotCreator99'>BotCreator99</a></b>\n
-<b>🔸Lᴀꜱᴛ ᴜᴘᴅᴀᴛᴇᴅ :- <a href='https://telegram.me/BotMinister'>[ 19-12-2022 ]</a></b>"""
+<b>🔸Vᴇʀꜱɪᴏɴ :- <a href='https://telegram.me/VJ_Bots'>1.0</a></b>\n
+<b>🔹Dᴇᴠᴇʟᴏᴘᴇʀ :- <a href='https://t.me/Anjel_Neha'>BotCreator99</a></b>\n
+<b>🔸Lᴀꜱᴛ ᴜᴘᴅᴀᴛᴇᴅ :- <a href='https://telegram.me/VJ_Bots'>[ 19-12-2022 ]</a></b>"""
